c24d488e46d4d8ce343d067d9262a0fd03b144cf
Sumit <panchalsumite2logy@gmail.com>
2024-12-17 11:00:58 +0530

    Cart Calculation

--- app/src/main/java/com/app/ecommerce/ui/base/view/SplashActivity.kt
+++ app/src/main/java/com/app/ecommerce/ui/base/view/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.app.ecommerce.R
 import com.app.ecommerce.ui.dashboard.view.DashBoardActivity
@@ -13,10 +14,12 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
 
-        Handler().postDelayed({
+        // Start Dashboard after 2 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, DashBoardActivity::class.java))
             finish()
-        }, 2000)
+        }, 5000)
     }
 }
